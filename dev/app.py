3d9cad9421d9102a40bc02e4ada97a9ceb0bc8be
JhonatanSmith <jhsgarciamu@unal.edu.co>
2024-07-28 19:23:29 -0500

    fix: Some buggs detected and fixed

--- dev/app.py
+++ dev/app.py
@@ -17,68 +17,82 @@ def uploaded_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        items = request.form.getlist('games')
-        items = [i for i in items if i.strip()]  # Remove empty or whitespace-only items
-        
-        if not items:
-            return render_template('index.html', message="No se ha insertado ningún juego para buscar.")
+        try:
+            items = request.form.getlist('games')
+            items = [i for i in items if i.strip()]  # Remove empty or whitespace-only items
+            
+            if not items:
+                return render_template('index.html', message="No se ha insertado ningún juego para buscar.")
+            
+            us_urls = [f"https://psdeals.net/us-store/search?search_query={i.replace(' ', '+')}" for i in items]
+            tr_urls = [f"https://psdeals.net/tr-store/search?search_query={i.replace(' ', '+')}" for i in items]
 
-        us_urls = [f"https://psdeals.net/us-store/search?search_query={i.replace(' ', '+')}" for i in items]
-        tr_urls = [f"https://psdeals.net/tr-store/search?search_query={i.replace(' ', '+')}" for i in items]
+            us_games_info = fetch_game_info(us_urls, items, 'USD')
+            tr_games_info = fetch_game_info(tr_urls, items, 'TRY')
 
-        us_games_info = fetch_game_info(us_urls, items, 'USD')
-        tr_games_info = fetch_game_info(tr_urls, items, 'TRY')
-
-        for game, details in tr_games_info.items():
-            for detail in details:
-                for us_detail in us_games_info.get(game, []):
-                    if detail['Platform'] == us_detail['Platform']:
-                        detail['Price (USD)'] = us_detail['Price (USD)']
-
-        data = []
-        for game, details in tr_games_info.items():
-            if details:  # Check if details list is not empty
+            for game, details in tr_games_info.items():
                 for detail in details:
-                    detail['Game'] = game
-                    data.append(detail)
-            else:
-                data.append({'Game': game, 'Platform': 'N/A', 'Price (TRY)': 'N/A', 'Price (USD)': 'N/A'})
+                    for us_detail in us_games_info.get(game, []):
+                        if detail['Platform'] == us_detail['Platform']:
+                            detail['Price (USD)'] = us_detail['Price (USD)']
+        except Exception as e:
+            return render_template('index.html', message=f"Error al procesar los juegos: {str(e)}")
 
-        if not data:
-            return render_template('index.html', message="No se han encontrado resultados para los juegos buscados.")
+        try:
+            data = []
+            for game, details in tr_games_info.items():
+                if details:  # Check if details list is not empty
+                    for detail in details:
+                        data.append(detail)
+                else:
+                    data.append({'Name': 'Not found', 'Platform': 'N/A', 'Price (TRY)': 'N/A', 'Price (USD)': 'N/A'})
 
-        df = pd.json_normalize(data)
+            if not data:
+                return render_template('index.html', message="No se han encontrado resultados para los juegos buscados.")
+        except Exception as e:
+            return render_template('index.html', message=f"Error al compilar los datos de los juegos: {str(e)}")
 
-        url = "https://www.google.com/search?q=lira+turca+a+dolar&oq=lira+turca+a+dolar&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARAuGEDSAQgyMzU4ajBqMagCALACAA&sourceid=chrome&ie=UTF-8"
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Accept-Encoding': 'identity',
-            'Connection': 'keep-alive'
-        }
-        r = requests.get(url, headers=headers)
-        soup = BeautifulSoup(r.text, "lxml")
-        requested_price = soup.find_all("span", class_="DFlfde SwHCTb")
-        dolar_tr = [price['data-value'] for price in requested_price]
-        dolar_tr = float(dolar_tr[0])
-        print("A dolar is equal to {} TRY".format(dolar_tr))
+        try:
+            df = pd.json_normalize(data)
 
-        # Clean and convert price columns
-        df["Price (TRY)"] = df["Price (TRY)"].str.replace("TL", "").str.replace("FREE", "0").str.replace(",", "").replace('N/A', '0')
-        df["Price (USD)"] = df["Price (USD)"].str.replace("FREE", "0").str.replace("$", "").replace('N/A', '0')
+            url = "https://www.google.com/search?q=lira+turca+a+dolar&oq=lira+turca+a+dolar&gs_lcrp=EgZjaHJvbWUqBggAEEUYOzIGCAAQRRg7MgYIARAuGEDSAQgyMzU4ajBqMagCALACAA&sourceid=chrome&ie=UTF-8"
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+                'Accept-Language': 'en-US,en;q=0.9',
+                'Accept-Encoding': 'identity',
+                'Connection': 'keep-alive'
+            }
+            r = requests.get(url, headers=headers)
+            soup = BeautifulSoup(r.text, "lxml")
+            requested_price = soup.find_all("span", class_="DFlfde SwHCTb")
+            dolar_tr = [price['data-value'] for price in requested_price]
+            dolar_tr = float(dolar_tr[0])
+            print("A dolar is equal to {} TRY".format(dolar_tr))
+        except Exception as e:
+            return render_template('index.html', message=f"Error al obtener la tasa de cambio: {str(e)}")
 
-        df["Price (USD)"] = pd.to_numeric(df["Price (USD)"], errors='coerce').fillna(0)
-        df["Price (TRY)"] = pd.to_numeric(df["Price (TRY)"], errors='coerce').fillna(0)
-        
-        df["Price (TRY - USD)"] = round(df["Price (TRY)"] * dolar_tr, 2)
-        df["Difference (US store - TR store)"] = round(df["Price (USD)"] - df["Price (TRY)"] * dolar_tr, 2)
-        df = df.iloc[:, [0, 1, 3, 5, 6, 2]]
+        try:
+            # Clean and convert price columns
+            df["Price (TRY)"] = df["Price (TRY)"].str.replace("TL", "").str.replace("FREE", "0").str.replace(",", "").replace('N/A', '0')
+            df["Price (USD)"] = df["Price (USD)"].str.replace("FREE", "0").str.replace("$", "").replace('N/A', '0')
 
-        return render_template('index.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
+            df["Price (USD)"] = pd.to_numeric(df["Price (USD)"], errors='coerce').fillna(0)
+            df["Price (TRY)"] = pd.to_numeric(df["Price (TRY)"], errors='coerce').fillna(0)
+            
+            df["Price (TRY - USD)"] = round(df["Price (TRY)"] * dolar_tr, 2)
+            df["Difference (US store - TR store)"] = round(df["Price (USD)"] - df["Price (TRY - USD)"], 2)
+            df = df.iloc[:, [0, 1, 2, 4, 5, 3]]  # Reorder columns to match your preference
+
+            return render_template('index.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
+        except KeyError as e:
+            return render_template('index.html', message=f"Error al limpiar y convertir columnas de precios: Columna no encontrada {str(e)}")
+        except Exception as e:
+            return render_template('index.html', message=f"Error al limpiar y convertir columnas de precios: {str(e)}")
     return render_template('index.html')
+
 def fetch_game_info(urls, items, region):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, como Gecko) Chrome/91.0.4472.124 Safari/537.36',
         'Accept-Language': 'en-US,en;q=0.9',
         'Accept-Encoding': 'identity',
         'Connection': 'keep-alive'
@@ -105,7 +119,12 @@ def fetch_game_info(urls, items, region):
                 if name_tag and platform_tag:
                     name = name_tag.get_text(strip=True)
                     platform = platform_tag.get_text(strip=True)
-                    price = price_discount_tag.get_text(strip=True) if price_discount_tag else price_tag.get_text(strip=True)
+                    if price_discount_tag:
+                        price = price_discount_tag.get_text(strip=True)
+                    elif price_tag:
+                        price = price_tag.get_text(strip=True)
+                    else:
+                        price = 'N/A'
                     product_details.append({
                         'Name': name,
                         f'Price ({region})': price,
@@ -123,6 +142,14 @@ def fetch_game_info(urls, items, region):
 
         except requests.RequestException as e:
             print(f"Error fetching data for {item} ({region}): {e}")
+            if item not in games_info:
+                games_info[item] = [{
+                    'Name': 'No encontrado',
+                    f'Price ({region})': 'N/A',
+                    'Platform': 'N/A'
+                }]
+        except Exception as e:
+            print(f"Error general procesando {item} ({region}): {str(e)}")
             if item not in games_info:
                 games_info[item] = [{
                     'Name': 'No encontrado',
