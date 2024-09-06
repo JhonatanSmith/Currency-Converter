78dc3e413011be77f2a80f2d0a2ebe50d722019d
JhonatanSmith <jhsgarciamu@unal.edu.co>
2024-09-05 19:01:26 -0500

    Test

--- dev/app.py
+++ dev/app.py
@@ -161,4 +161,4 @@ def fetch_game_info(urls, items, region):
 
 if __name__ == '__main__':
     # Using any IP to connect to this data when deployed
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
