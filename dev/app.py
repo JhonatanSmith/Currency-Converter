5866fa37ce36acd86d7981a30860d3c9b5aa2c2e
JhonatanSmith <jhsgarciamu@unal.edu.co>
2024-09-05 19:57:35 -0500

    Fix: new Render deployment slution applied

--- dev/app.py
+++ dev/app.py
@@ -161,4 +161,5 @@ def fetch_game_info(urls, items, region):
 
 if __name__ == '__main__':
     # Using any IP to connect to this data when deployed
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
